1c13b6fc08024362019a9d412ee93ed4993bbddd
SOUTOVACWAY <59883919+SOUTOVACWAY@users.noreply.github.com>
2022-01-31 09:57:14 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { store, history } from './store'
+import Root from './containers/Root'
+import { authLoginSuccess } from './actions/auth'
 
+const target = document.getElementById('root')
+
+const root = (
+  <Root store={store} history={history} />
+)
+
+const token = localStorage.getItem('token')
+if (token !== null) {
+  store.dispatch(authLoginSuccess(token))
+}
+
+ReactDOM.render(root, target)
